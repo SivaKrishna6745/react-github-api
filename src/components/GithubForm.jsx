a0569d16ada28303c13beb5929a5183a34c6f9fc
SivaKrishna6745 <krishnasiva6745@gmail.com>
2022-08-04 22:59:09 +0530

    search form with styles

---
+++ src/components/GithubForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function GithubForm() {
+    return (
+        <form className="form-group">
+            <input type="text" className="form-control" />
+            <button className="btn">Search</button>
+        </form>
+    );
+}
+
+export default GithubForm;
