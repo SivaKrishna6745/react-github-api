fe02eb8af59667272e29a18411687ac9836198c7
SivaKrishna6745 <krishnasiva6745@gmail.com>
2022-08-06 20:08:26 +0530

    github form search with axios fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import { useState } from "react";
 import "./App.css";
 import GithubForm from "./components/GithubForm.jsx";
 
 function App() {
+    const [res, setRes] = useState({});
     return (
         <div className="App">
             <h1>Hello Elliot!!</h1>
-            <GithubForm />
+            <GithubForm res={res} setRes={setRes} />
         </div>
     );
 }
--- src/components/GithubForm.jsx
+++ src/components/GithubForm.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
-function GithubForm() {
+function GithubForm({ res, setRes }) {
+    const [search, setSearch] = useState("");
+    function handleChange(e) {
+        setSearch(e.target.value);
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+        axios
+            .get(`https://api.github.com/users/${search}`)
+            .then((response) => {
+                const { data } = response;
+                setRes(data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
     return (
-        <form className="form-group">
-            <input type="text" className="form-control" />
+        <form className="form-group" onSubmit={handleSubmit}>
+            <input
+                type="text"
+                className="form-control"
+                value={search}
+                onChange={handleChange}
+            />
             <button className="btn">Search</button>
         </form>
     );
